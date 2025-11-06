08a93e1d44c6eb4ffa5cb1ff2867c12de4bf90ed
KitKat <227429303+kitkat6464@users.noreply.github.com>
2025-11-06 19:55:35 +0000

    Mmmmmm I smell progress, no wait my house on fire

--- desktop.sh
+++ desktop.sh
@@ -52,6 +52,97 @@ cat /etc/pam.d/greetd
 
 systemctl enable greetd
 
+#install packages. we run and remove any on this list if its preinstalled.
+dnf -y install \
+    brightnessctl \
+    cava \
+    chezmoi \
+    ddcutil \
+    fastfetch \
+    flatpak \
+    fpaste \
+    fzf \
+    git-core \
+    glycin-thumbnailer \
+    gnome-keyring \
+    greetd \
+    greetd-selinux \
+    input-remapper \
+    just \
+    dolphin \
+    orca \
+    pipewire \
+    steam-devices \
+    tuigreet \
+    udiskie \
+    webp-pixbuf-loader \
+    wireplumber \
+    wl-clipboard \
+    wlsunset \
+    xdg-desktop-portal-gnome \
+    xdg-desktop-portal-kde \
+    xdg-user-dirs \
+    xwayland-satellite
+
+dnf install -y --setopt=install_weak_deps=False \
+    kf6-kirigami \
+    qt6ct \
+    polkit-kde \
+    plasma-breeze \
+    kf6-qqc2-desktop-style
+
+#session target thing
+sed -i "s/After=.*/After=graphical-session.target/" /usr/lib/systemd/user/plasma-polkit-agent.service
+
+#kde dialogs
+tee /usr/share/xdg-desktop-portal/niri-portals.conf <<'EOF'
+[preferred]
+default=kde;gnome;
+org.freedesktop.impl.portal.ScreenCast=gnome;
+org.freedesktop.impl.portal.Access=kde;
+org.freedesktop.impl.portal.Notification=kde;
+org.freedesktop.impl.portal.Secret=gnome-keyring;
+EOF
+
+#add systemd stuff
+add_wants_niri() {
+    sed -i "s/\[Unit\]/\[Unit\]\nWants=$1/" "/usr/lib/systemd/user/niri.service"
+}
+add_wants_niri cliphist.service
+add_wants_niri plasma-polkit-agent.service
+add_wants_niri udiskie.service
+add_wants_niri xwayland-satellite.service
+cat /usr/lib/systemd/user/niri.service
+
+systemctl enable --global dms.service
+systemctl enable --global cliphist.service
+systemctl enable --global gnome-keyring-daemon.socket
+systemctl enable --global gnome-keyring-daemon.service
+systemctl enable --global plasma-polkit-agent.service
+systemctl enable --global udiskie.service
+systemctl enable --global xwayland-satellite.service
+systemctl preset --global cliphist
+systemctl preset --global plasma-polkit-agent
+systemctl preset --global udiskie
+systemctl preset --global xwayland-satellite
+
+#fonts
+dnf install -y \
+    default-fonts-core-emoji \
+    google-noto-color-emoji-fonts \
+    google-noto-emoji-fonts \
+    glibc-all-langpacks \
+    default-fonts
+
+mkdir -p "/usr/share/fonts/Maple Mono"
+
+MAPLE_TMPDIR="$(mktemp -d)"
+trap 'rm -rf "${MAPLE_TMPDIR}"' EXIT
+
+LATEST_RELEASE_FONT="$(curl "https://api.github.com/repos/subframe7536/maple-font/releases/latest" | jq '.assets[] | select(.name == "MapleMono-Variable.zip") | .browser_download_url' -rc)"
+curl -fSsLo "${MAPLE_TMPDIR}/maple.zip" "${LATEST_RELEASE_FONT}"
+unzip "${MAPLE_TMPDIR}/maple.zip" -d "/usr/share/fonts/Maple Mono"
+
 #will add stuff as I learn
 
 #clean that dnf up
